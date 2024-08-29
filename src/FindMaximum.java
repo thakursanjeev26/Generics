f46b084acbced2bc2c726797a3518a53963ae608
thakursanjeev26 <sanjeevjthakur@gmail.com>
2024-08-29 12:55:25 +0530

    Added:UC3_Find_Max_Of_3_Strings

--- src/FindMaximum.java
+++ src/FindMaximum.java
@@ -31,6 +31,22 @@ public class FindMaximum {
         return max;
     }
 
+    public static  String findMax(String a,String b,String c){
+
+        String max = a;
+
+        if(b.compareTo(max)>0){
+            max = b;
+        }
+
+        if(c.compareTo(max)>0){
+            max = c;
+        }
+
+        return max;
+
+    }
+
     public static void main(String[] args) {
 
         System.out.println("Welcome To Generics");
@@ -42,6 +58,10 @@ public class FindMaximum {
         Float x1=10.5f,y1=20.8f,z1=30.2f;
 
         System.out.println("Maximum_Of_3_Float: "+findMax(x1,y1,z1));
+
+        String s1="Apple",s2="Banana",s3="Peach";
+
+        System.out.println("Maximum_Of_3_Strings: "+findMax(s1,s2,s3));
 
 
     }
