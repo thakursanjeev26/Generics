b69226dfcff3bf4aa3c97faec7d704734f5dbff2
thakursanjeev26 <sanjeevjthakur@gmail.com>
2024-08-29 15:28:38 +0530

    Added:Refactor1

--- src/FindMaximum.java
+++ src/FindMaximum.java
@@ -1,24 +1,7 @@
 public class FindMaximum {
 
-    public static Integer findMax(Integer a,Integer b,Integer c){
-
-        Integer max = a;
-
-        if(b.compareTo(max)>0){
-            max=b;
-        }
-
-        if(c.compareTo(max)>0){
-            max=c;
-        }
-
-        return max;
-
-    }
-
-    public static Float findMax(Float a,Float b,Float c){
-
-        Float max = a;
+    public static <T extends Comparable<T>>T findMax(T a,T b,T c){
+        T max = a;
 
         if(b.compareTo(max)>0){
             max=b;
@@ -29,22 +12,6 @@ public class FindMaximum {
         }
 
         return max;
-    }
-
-    public static  String findMax(String a,String b,String c){
-
-        String max = a;
-
-        if(b.compareTo(max)>0){
-            max = b;
-        }
-
-        if(c.compareTo(max)>0){
-            max = c;
-        }
-
-        return max;
-
     }
 
     public static void main(String[] args) {
