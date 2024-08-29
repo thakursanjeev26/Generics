e8d791e7ad889e61c842f25eb9840f3f7047ef9a
thakursanjeev26 <sanjeevjthakur@gmail.com>
2024-08-29 11:46:22 +0530

    Added:UC1_Find_Max_Of_3_Integers

--- src/FindMaximum.java
+++ src/FindMaximum.java
@@ -1,5 +1,29 @@
 public class FindMaximum {
+
+    public static Integer findMax(Integer a,Integer b,Integer c){
+
+        Integer max = a;
+
+        if(b.compareTo(max)>0){
+            max=b;
+        }
+
+        if(c.compareTo(max)>0){
+            max=c;
+        }
+
+        return max;
+
+    }
+
     public static void main(String[] args) {
+
         System.out.println("Welcome To Generics");
+
+        Integer a1=10,b1=20,c1=30;
+
+        System.out.println("Maximum_Of_3_Integers: "+findMax(a1,b1,c1));
+
+
     }
 }
