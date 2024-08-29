9e7807a2d4fdb2874614992ed19a76614787f1b9
thakursanjeev26 <sanjeevjthakur@gmail.com>
2024-08-29 11:53:25 +0530

    Added:UC2_Find_Max_Of_3_Float

--- src/FindMaximum.java
+++ src/FindMaximum.java
@@ -16,6 +16,21 @@ public class FindMaximum {
 
     }
 
+    public static Float findMax(Float a,Float b,Float c){
+
+        Float max = a;
+
+        if(b.compareTo(max)>0){
+            max=b;
+        }
+
+        if(c.compareTo(max)>0){
+            max=c;
+        }
+
+        return max;
+    }
+
     public static void main(String[] args) {
 
         System.out.println("Welcome To Generics");
@@ -23,6 +38,10 @@ public class FindMaximum {
         Integer a1=10,b1=20,c1=30;
 
         System.out.println("Maximum_Of_3_Integers: "+findMax(a1,b1,c1));
+
+        Float x1=10.5f,y1=20.8f,z1=30.2f;
+
+        System.out.println("Maximum_Of_3_Float: "+findMax(x1,y1,z1));
 
 
     }
